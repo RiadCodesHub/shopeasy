dd6b9e83113ef36247cde5820f81afccb0a9ce59
Riad <riad75599@gmail.com>
2026-03-10 00:57:55 +0600

    debbuging error

--- app/deals/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DealsPage() {
-  return <div>Deals page coming soon</div>;
-}
--- app/forgot-password/page.tsx
+++
@@ -1,5 +0,0 @@
-
-
-export default function ForgotPassword() {
-    return <div> Page coming soon</div>
-}
