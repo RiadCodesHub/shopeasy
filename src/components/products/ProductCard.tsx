8536ed86dff228298d18746cee6df91796d25cbf
Riad <riad75599@gmail.com>
2026-03-26 00:29:31 +0600

    fix productcard type

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -23,6 +23,7 @@ const ProductCard = ({product, index} : ProductCardProps) => {
         name: product.name,
         price: product.price,
         image: product.image,
+        quantity: 1
     }));
     };
 
